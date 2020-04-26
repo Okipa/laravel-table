32f593d2adb355995f8a0914f8463e5a593f6141
Arthur LORENT <arthur@acid.fr>
2020-04-26 15:21:38 +0200

    wip

--- tests/Fakers/UsersFaker.php
+++ tests/Fakers/UsersFaker.php
@@ -31,8 +31,8 @@ trait UsersFaker
         $this->clearPassword = $this->faker->word;
 
         return [
-            'name'     => $this->faker->word(10),
-            'email'    => $this->faker->email,
+            'name'     => $this->faker->unique()->name(),
+            'email'    => $this->faker->unique()->email,
             'password' => Hash::make($this->clearPassword),
         ];
     }
