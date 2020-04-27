5682993da9e7c55cc61d36a57d76973987d27fb1
Arthur LORENT <arthur@acid.fr>
2020-04-27 18:23:13 +0200

    Finally removed useless and confusing Table facade

--- src/Abstracts/AbstractTable.php
+++ src/Abstracts/AbstractTable.php
@@ -6,18 +6,39 @@ use Okipa\LaravelTable\Table;
 
 abstract class AbstractTable
 {
-    public function __construct()
+    public function __invoke(): Table
     {
         $table = $this->table();
         $this->columns($table);
         $this->resultLines($table);
+
+        return $table;
     }
 
+    /**
+     * Configure the table itself.
+     *
+     * @return \Okipa\LaravelTable\Table
+     * @throws \ErrorException
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     */
     abstract protected function table(): Table;
 
+    /**
+     * Configure the table columns.
+     *
+     * @param \Okipa\LaravelTable\Table $table
+     *
+     * @throws \ErrorException
+     */
     abstract protected function columns(Table $table): void;
 
-    /** @SuppressWarnings(PHPMD.UnusedFormalParameter) */
+    /**
+     * Configure the table result lines.
+     *
+     * @param \Okipa\LaravelTable\Table $table
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
     protected function resultLines(Table $table): void
     {
         //
