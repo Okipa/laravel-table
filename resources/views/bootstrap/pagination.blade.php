b61b3fcd30c95929b031d61ba537eae7730e4843
Brian Matovu <mtvbrianking@gmail.com>
2020-12-07 19:55:41 +0300

    Fix pagination margin bottom
    The default `mb-n3`  pushes the margin down.
    
    Removing it seems to fix the issue

--- resources/views/bootstrap/pagination.blade.php
+++ resources/views/bootstrap/pagination.blade.php
@@ -1,3 +1,3 @@
-<div class="d-flex align-items-center mb-n3 px-3 py-1 pagination-container">
+<div class="d-flex align-items-center px-3 py-1 pagination-container">
     {!! $table->getPaginator()->links() !!}
 </div>
