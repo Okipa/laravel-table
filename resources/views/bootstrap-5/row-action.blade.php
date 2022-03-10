eeb739da0614133258010ff3f37573caa3b29a09
Artu1000 <arthur.lorent@gmail.com>
2022-03-10 08:45:12 +0100

    WIP

--- resources/views/bootstrap-5/row-action.blade.php
+++ resources/views/bootstrap-5/row-action.blade.php
@@ -1,5 +1,5 @@
 <a wire:click.prevent="rowAction('{{ $key }}', {{ $modelKey }}, {{ $shouldBeConfirmed ? 1 : 0 }})"
-   @class([$class, 'p-2'])
+   @class([$class, 'p-1'])
    href=""
    title="{{ $title }}">
     {!! $icon !!}
