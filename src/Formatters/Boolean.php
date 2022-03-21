3855dd86107c8951c15078698761c86a051c6a30
Artu1000 <arthur.lorent@gmail.com>
2022-03-21 20:57:50 +0100

    WIP

--- src/Formatters/Boolean.php
+++ src/Formatters/Boolean.php
@@ -10,7 +10,7 @@ class Boolean extends AbstractFormatter
     public function format(Model $model, string $attribute): string
     {
         return $model->{$attribute}
-            ? '<span class="text-success">' . config('laravel-table.icon.active' ). '</span>'
+            ? '<span class="text-success">' . config('laravel-table.icon.active') . '</span>'
             : '<span class="text-danger">' . config('laravel-table.icon.inactive') . '</span>';
     }
 }
