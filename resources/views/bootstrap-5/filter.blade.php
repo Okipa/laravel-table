af0c1902a1b961139cc6ab832e582bb2f26f32a3
Artu1000 <arthur.lorent@gmail.com>
2022-05-09 23:12:19 +0200

    WIP

--- resources/views/bootstrap-5/filter.blade.php
+++ resources/views/bootstrap-5/filter.blade.php
@@ -1,4 +1,5 @@
-<select wire:change="filter('{{ $filter->identifier }}', $event.target.value)"
+<select wire:key="filter-{{ $filter->identifier }}"
+        wire:change="filter('{{ $filter->identifier }}', $event.target.value)"
         @class(['form-select', $filter->class => $filter->class])
         {!! $filter->multiple ? 'multiple' : null !!}
         aria-label="{{ $filter->label }}">
