a21961a09e909b41d4da94d1be5f8b385fbf2f4b
Artu1000 <arthur.lorent@gmail.com>
2022-05-11 00:08:25 +0200

    WIP

--- resources/views/bootstrap-5/filter.blade.php
+++ resources/views/bootstrap-5/filter.blade.php
@@ -1,10 +1,9 @@
 <div wire:key="filter-{{ Str::slug($filter->identifier) }}">
     <select wire:model="selectedFilters.{{ $filter->identifier }}"
-            {{-- wire:change="filter('{{ $filter->identifier }}', $event.target.value)" --}}
             @class(['form-select', $filter->class => $filter->class])
             {!! $filter->multiple ? 'multiple' : null !!}
             aria-label="{{ $filter->label }}">
-        <option wire:key="filter-option-{{ Str::slug($filter->identifier) }}-placeholder" selected>{{ $filter->label }}</option>
+        <option wire:key="filter-option-{{ Str::slug($filter->identifier) }}-placeholder" value="" selected>{{ $filter->label }}</option>
         @foreach($filter->options as $optionValue => $optionLabel)
             <option wire:key="filter-option-{{ Str::slug($filter->identifier) }}-{{ Str::slug($optionValue) }}" value="{{ $optionValue }}">{{ $optionLabel }}</option>
         @endforeach
