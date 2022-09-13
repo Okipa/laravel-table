95223b8bef603364f38afba679b668f1b2f36747
Arthur LORENT <arthur@acid.fr>
2022-09-13 17:03:18 +0200

    Improved row class generation by generating a unique identifier for each model, allowing to manage row classes for model without IDs

--- src/Table.php
+++ src/Table.php
@@ -307,7 +307,7 @@ class Table
     public function paginateRows(Builder $query, int $numberOfRowsPerPage): void
     {
         $this->rows = $query->paginate($numberOfRowsPerPage);
-        $this->rows->transform(function(Model $model) {
+        $this->rows->transform(function (Model $model) {
             $model->laravel_table_unique_identifier = Str::uuid()->getInteger()->toString();
 
             return $model;
