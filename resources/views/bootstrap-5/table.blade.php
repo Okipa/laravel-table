3945cb5de2f5182dde9ffef1dbfee3103b5ade8f
Adriano Castro <72993300+acastro1-godaddy@users.noreply.github.com>
2023-11-01 15:57:29 -0300

    Update blades to print the new headerClass property

--- resources/views/bootstrap-5/table.blade.php
+++ resources/views/bootstrap-5/table.blade.php
@@ -138,7 +138,7 @@
                         @endif
                         {{-- Sorting/Column titles --}}
                         @foreach($columns as $column)
-                            <th wire:key="column-{{ Str::of($column->getAttribute())->snake('-')->slug() }}" class="align-middle" scope="col">
+                            <th wire:key="column-{{ Str::of($column->getAttribute())->snake('-')->slug() }}" class="align-middle {{ $column->getHeaderClass() }}" scope="col">
                                 @if($column->isSortable($orderColumn))
                                     @if($sortBy === $column->getAttribute())
                                         <a wire:click.prevent="sortBy('{{ $column->getAttribute() }}')"
